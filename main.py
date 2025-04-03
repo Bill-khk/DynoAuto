9e3ca89905bf4f2b7feab1b0ed5313919b76f0fe
Bill Khk <bill.khk@gmail.com>
2025-04-03 19:19:54 +0700

    Test fuction created using pynput

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pyautogui
 from PIL import Image
 import time
 
+from pynput import keyboard
 
-time.sleep(5)
-playing = True
+#time.sleep(5)
+playing = False
 
-# TODO create a version that screenshot and get the level of screen everytime space is pressed
+# TODO Put screenshot and color analysis into functions
 while playing:
     img = pyautogui.screenshot('screen.png', region=(200, 600, 300, 300))
     pil_img = Image.open('screen.png').convert('L')  # Open in Grey scale
@@ -25,3 +26,26 @@ while playing:
         pyautogui.press('space')
         time.sleep(0.5)
 
+
+def on_press(key):
+    if key == keyboard.Key.space:
+        pyautogui.screenshot('test.png', region=(200, 600, 300, 300))
+        pil_img = Image.open('test.png').convert('L')  # Open in Grey scale
+        width, height = pil_img.size
+
+        pixel_nb = 0
+        total_value = 0
+        for x in range(width):
+            for y in range(height):
+                value = pil_img.getpixel((x, y))
+                total_value += pil_img.getpixel((x, y))
+                pixel_nb += 1
+        mean_black = total_value / pixel_nb
+        print(mean_black)
+
+
+test = True
+while test:
+    with keyboard.Listener(
+            on_press=on_press) as listener:
+        listener.join()
