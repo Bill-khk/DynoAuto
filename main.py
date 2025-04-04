5ff4f248b84a7bcbffa46d574fb2b1577e31e4ee
Bill Khk <bill.khk@gmail.com>
2025-04-04 11:27:22 +0700

    Code overhaul:Optimization, OOP, mergin playing and test function

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pyautogui
 from PIL import Image
 import time
-
 from pynput import keyboard
 
-#time.sleep(5)
-playing = False
 
-# TODO Put screenshot and color analysis into functions
-while playing:
-    img = pyautogui.screenshot('screen.png', region=(200, 600, 300, 300))
+def take_screen():
+    pyautogui.screenshot('screen.png', region=(200, 600, 300, 300))
+
+
+def get_grey_value():
     pil_img = Image.open('screen.png').convert('L')  # Open in Grey scale
     width, height = pil_img.size
 
@@ -17,35 +16,38 @@ while playing:
     total_value = 0
     for x in range(width):
         for y in range(height):
-            value = pil_img.getpixel((x, y))
             total_value += pil_img.getpixel((x, y))
             pixel_nb += 1
     mean_black = total_value / pixel_nb
     print(mean_black)
     if mean_black < 251:
-        pyautogui.press('space')
-        time.sleep(0.5)
+        global test
+        if not test:
+            jump()
+
+
+def jump():
+    pyautogui.press('space')
 
 
 def on_press(key):
     if key == keyboard.Key.space:
-        pyautogui.screenshot('test.png', region=(200, 600, 300, 300))
-        pil_img = Image.open('test.png').convert('L')  # Open in Grey scale
-        width, height = pil_img.size
-
-        pixel_nb = 0
-        total_value = 0
-        for x in range(width):
-            for y in range(height):
-                value = pil_img.getpixel((x, y))
-                total_value += pil_img.getpixel((x, y))
-                pixel_nb += 1
-        mean_black = total_value / pixel_nb
-        print(mean_black)
+        print('Space')
+        take_screen()
+        get_grey_value()
 
 
+# ---------------- Main function -----------------------
+playing = True
 test = True
-while test:
-    with keyboard.Listener(
-            on_press=on_press) as listener:
-        listener.join()
+# Create listener on Keyboard
+# TODO using thread to manage the listener and the while loop
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
+
+while playing:
+    # Start taking screen
+    take_screen()
+    get_grey_value()
+    time.sleep(0.1)
